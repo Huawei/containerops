03aed05e1bdfd467e306d08289a5cebd57a02d0d
谢微微 <xieweiwei@wboll.com>
2016-10-29 13:26:33 +0800

    format app javascript description & Increase history pipeline

--- scaffold/src/app/index.js
+++ scaffold/src/app/index.js
@@ -1,16 +1,22 @@
-/* Copyright 2014 Huawei Technologies Co., Ltd. All rights reserved.
+/* 
+Copyright 2014 Huawei Technologies Co., Ltd. All rights reserved.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
-limitations under the License. */
+limitations under the License.
+ */
 
 import {initPipelinePage} from "./pipeline/main";
 import {initComponentPage} from "./component/main";
+import {initHistoryPage} from "./history/main";
 
 $._messengerDefaults = {
     extraClasses: 'messenger-fixed messenger-theme-future messenger-on-top'
@@ -26,6 +32,12 @@ $(".menu-pipeline").on('click',function(event){
 
 $(".menu-component").on('click',function(){
     initComponentPage();
+    $(event.currentTarget).parent().parent().children().removeClass("active");
+    $(event.currentTarget).parent().addClass("active");
+})
+
+$(".menu-history").on('click',function(){
+    initHistoryPage();
     $(event.currentTarget).parent().parent().children().removeClass("active");
     $(event.currentTarget).parent().addClass("active");
 })
