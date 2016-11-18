d76dc736f09eb1193027681e07e93cc087237742
ZhengYongJing <yongjingzheng1983#gmail.com>
2016-11-18 15:49:45 +0800

    add gulp watch task

--- tenant/gulpfile.js
+++ tenant/gulpfile.js
@@ -8,10 +8,16 @@ let sass = require('gulp-sass');
 /**
  *  This will compile scss to css
  */
-gulp.task('default', function() {
-    return gulp.src('./sass/**/*.{scss,css}', { base: "./sass" })
+gulp.task("dev:watch", () => {
+    gulp.watch("./sass/**/*.{scss,css}", ['dev:style']);
+});
+gulp.task("dev:style", () => {
+	return gulp.src('./sass/**/*.{scss,css}', { base: "./sass" })
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css/'));
-});
+})
+gulp.task("default", () => {
+	gulp.start('dev:watch','dev:style');
+})
 
 
