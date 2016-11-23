65f131dbfae3ca76931289623ac8b1d6e5d40a12
ZhengYongJing <yongjingzheng1983@gmail.com>
2016-11-23 11:07:03 +0800

    modify the webpack config file and css

--- tenant/src/app/app.component.ts
+++ tenant/src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 // import { NavComponent } from './nav.component';
 // import { RepoService } from './repo.service';
 
 
 @Component({
-  selector: 'my-app',
+  selector: 'body',
   template: `<router-outlet></router-outlet>`,
   // template: require('./app.html'),
-  styles: [require('../sass/application.scss')]
+  styles: [require('../sass/application.scss')],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent { 
 	constructor() {}
