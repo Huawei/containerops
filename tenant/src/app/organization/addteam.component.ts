a6365cc9c53dde84674a9814956a9823e49b18d2
ChengXi <xcheng@linkernetworks.com>
2016-11-25 16:42:20 +0800

    bugfix,add team data service, add team into org

--- tenant/src/app/organization/addteam.component.ts
+++ tenant/src/app/organization/addteam.component.ts
@@ -4,13 +4,16 @@ import {OrgDataService} from './data.service';
 import {TeamDataService} from '../team/data.service';
 
 @Component({
-  selector: 'org-detail',
-  templateUrl: '../../template/organization/detail.html'
+  selector: 'org-add-team',
+  templateUrl: '../../template/organization/addTeam.html'
 })
 
-export class OrgDetailComponent implements OnInit { 
+export class OrgAddTeamComponent implements OnInit { 
 	org;
-	teams;
+	team = {
+		"name" : "",
+		"desc" : ""
+	};
 
 	constructor(private router: Router,
 				private route: ActivatedRoute,
@@ -28,15 +31,23 @@ export class OrgDetailComponent implements OnInit {
 	      let id = +params['id'];
 	      this.org = this.orgDataService.getOrg(id);
 	    });
-
-	    this.getTeams();
 	}
 
-	getTeams(): void{
-		this.teams = this.teamDataService.getTeams(this.org.id);
+	addTeam(): void {
+		try{
+			//fake, to be deleted
+			this.team["orgid"] = this.org.id;
+			this.teamDataService.addTeam(this.team);
+			alert("team "+ this.team.name + " added.");
+
+			this.router.navigate(['/organization', this.org.id]);
+			//fake end
+		}catch(e){
+			alert("failed to add team.")
+		}
 	}
 
-	showNewTeam(): void{
-		this.router.navigate(['/organization', this.org.id, "addteam"]);
-	}
+	cancelAdd(): void{
+		this.router.navigate(['/organization', this.org.id]);
+	}	
 }
