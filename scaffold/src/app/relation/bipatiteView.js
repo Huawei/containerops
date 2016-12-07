62c5f985b97bfd380c76efa80139e8a5d9704098
roboWang <jxhz1989@163.com>
2016-12-07 16:01:50 +0800

    clear both

--- scaffold/src/app/relation/bipatiteView.js
+++ scaffold/src/app/relation/bipatiteView.js
@@ -97,7 +97,7 @@ function initView(importTree,outputTree,linePathData){
 		relationLineInit(linePathData.relation[startKey]);
 	}
 	
-	
+	var mouseX,mouseY;
 	
 	$("span.property").mousedown(function(event){
 
@@ -109,16 +109,19 @@ function initView(importTree,outputTree,linePathData){
 		var _startX = $(event.target).offset().left,
 	        _startY = $(event.target).offset().top,
 	        startClass = $(event.target).parent().attr("class"),
-	    	fromPath = $(event.target).parent().attr("data-path").replace(/\-/g,'.');
+	    	fromPath = $(event.target).parent().attr("data-path").replace(/\-/g,'.'),
 	    	fromPath = fromPath.substring(5);
+	    	mouseX = event.clientX;
+	    	mouseY = event.clientY;
 		
 	    document.onmousemove = function(event){
-	    	event.pageX
-	    	event.pageY
 	    	dragDropLine([_startX,_startY,event.pageX,event.pageY]);
 	    }
 
 	    document.onmouseup = function(event){
+	    	if(mouseX == event.clientX && mouseY == event.clientY){
+	    		return false;
+	    	}
 	    	document.onmousemove = null;   
         	document.onmouseup = null; 
         	
@@ -152,7 +155,12 @@ function initView(importTree,outputTree,linePathData){
 	    		
 	    	}
 	    }
-	})
+	}).mouseup(function(event){
+		if(mouseX == event.clientX && mouseY == event.clientY){
+    		document.onmousemove = null; 
+    		document.onmouseup = null; 
+    	}
+	});
 
 	
 
