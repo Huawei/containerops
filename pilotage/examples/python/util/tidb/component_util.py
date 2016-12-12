0bed23439356579ecbb81558c7c441ec5d83db21
xiechuanj <xiechuanj@gmail.com>
2016-12-12 08:58:25 +0800

    change log format

--- pilotage/examples/python/util/tidb/component_util.py
+++ pilotage/examples/python/util/tidb/component_util.py
@@ -1,10 +1,10 @@
 #encoding=utf-8
-             
+
 import logging, logging.handlers, json, urllib2, urllib, os, io, commands, shutil, time
 from BaseHTTPServer import BaseHTTPRequestHandler
- 
+
 # define const
-  
+
 LOG_FILE = 'tst.log'
 CO_POD_NAME     = "CO_POD_NAME"
 CO_RUN_ID       = "CO_RUN_ID"
@@ -20,7 +20,7 @@ CO_DATA = "CO_DATA"
 CO_SET_GLOBAL_VAR_URL = "CO_SET_GLOBAL_VAR_URL"
 CO_LINKSTART_TOKEN = "CO_LINKSTART_TOKEN"
 CO_LINKSTART_URL   = "CO_LINKSTART_URL"
-
+INFO = "INFO"
 #define info
 CO_INFO = {}
 
@@ -48,8 +48,7 @@ def init():
     CO_INFO[CO_REGISTER_URL] = os.getenv("CO_REGISTER_URL")
     CO_INFO[CO_EVENT_LIST] = os.getenv("CO_EVENT_LIST")
     CO_INFO[CO_RUN_ID] = os.getenv("CO_RUN_ID")
-    CO_INFO[CO_POD_NAME] = os.getenv("CO_POD_NAME") 
-    CO_INFO['CO_status'] = True   
+    CO_INFO[CO_POD_NAME] = os.getenv("CO_POD_NAME")
     eventlsts = CO_INFO[CO_EVENT_LIST].split(";")
     events = {}
     for eventlst in eventlsts:
@@ -64,47 +63,47 @@ def NotifyEvent(url, paras):
     request.add_header('Content-Type', 'application/json')
     request.get_method = lambda:'POST'
     request = urllib2.urlopen(request)
-    result = request.read() 
+    result = request.read()
     logger.debug("%s,%s,%s"%(url,paras,result))
-    return result  
-
-
-def  ComponentStart():    
-    paras = {"RUN_ID":CO_INFO[CO_RUN_ID],"EVENT":CO_COMPONENT_START,"EVENT_ID":int(CO_INFO[CO_EVENT_LIST][CO_COMPONENT_START])}   
-    result = NotifyEvent(CO_INFO[CO_COMPONENT_START], paras) 
     return result
 
 
-def  ComponentStop():    
-    paras = {"RUN_ID":CO_INFO[CO_RUN_ID],"EVENT":CO_COMPONENT_STOP,"EVENT_ID":int(CO_INFO[CO_EVENT_LIST][CO_COMPONENT_STOP])}    
+def  ComponentStart(output="",result="component start ...",status=True):
+    paras = {"RUN_ID":CO_INFO[CO_RUN_ID],"EVENT":CO_COMPONENT_START,"EVENT_ID":int(CO_INFO[CO_EVENT_LIST][CO_COMPONENT_START]), INFO:{"output":output,"result":result,"status":status}}
+    result = NotifyEvent(CO_INFO[CO_COMPONENT_START], paras)
+    return result
+
+
+def  ComponentStop(output="",result="component stop ...",status=True):
+    paras = {"RUN_ID":CO_INFO[CO_RUN_ID],"EVENT":CO_COMPONENT_STOP,"EVENT_ID":int(CO_INFO[CO_EVENT_LIST][CO_COMPONENT_STOP]), INFO:{"output":output,"result":result,"status":status}}
     result = NotifyEvent(CO_INFO[CO_COMPONENT_STOP], paras)
-    return result      
+    return result
 
-def  TaskStart():    
-    paras = {"RUN_ID":CO_INFO[CO_RUN_ID],"EVENT":CO_TASK_START,"EVENT_ID":int(CO_INFO[CO_EVENT_LIST][CO_TASK_START])}    
-    result = NotifyEvent(CO_INFO[CO_TASK_START], paras)    
-    return result  
+def  TaskStart(output="",result="task start ...",status=True):
+    paras = {"RUN_ID":CO_INFO[CO_RUN_ID],"EVENT":CO_TASK_START,"EVENT_ID":int(CO_INFO[CO_EVENT_LIST][CO_TASK_START]), INFO:{"output":output,"result":result,"status":status}}
+    result = NotifyEvent(CO_INFO[CO_TASK_START], paras)
+    return result
 
-def  TaskResult(info={}):    
-    paras = {"RUN_ID":CO_INFO[CO_RUN_ID],"EVENT":CO_TASK_RESULT,"EVENT_ID":int(CO_INFO[CO_EVENT_LIST][CO_TASK_RESULT]),"INFO":info}    
+def  TaskResult(output="",result="task result ...",status=True):
+    paras = {"RUN_ID":CO_INFO[CO_RUN_ID],"EVENT":CO_TASK_RESULT,"EVENT_ID":int(CO_INFO[CO_EVENT_LIST][CO_TASK_RESULT]), INFO:{"output":output,"result":result,"status":status}}
     result = NotifyEvent(CO_INFO[CO_TASK_RESULT], paras)
-    return result  
+    return result
 
-def  TaskStatus(info={}):    
-    paras = {"RUN_ID":CO_INFO[CO_RUN_ID],"EVENT":CO_TASK_STATUS,"EVENT_ID":int(CO_INFO[CO_EVENT_LIST][CO_TASK_STATUS]), "INFO":info}    
+def  TaskStatus(output="",result="task status ...",status=True):
+    paras = {"RUN_ID":CO_INFO[CO_RUN_ID],"EVENT":CO_TASK_STATUS,"EVENT_ID":int(CO_INFO[CO_EVENT_LIST][CO_TASK_STATUS]), INFO:{"output":output,"result":result,"status":status}}
     result = NotifyEvent(CO_INFO[CO_TASK_STATUS], paras)
-    return result  
+    return result
 
 
 def execCommand(cmd):
-    TaskResult({"cmd":cmd, "path":os.getcwd()})
+    TaskResult(output=cmd, result=os.getcwd())
     import subprocess
     subp=subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
     while subp.poll()==None:
         message = subp.stdout.readline()
         logger.debug(message)
-        TaskResult({"result":message,})
-     
+        TaskResult(result=message)
+
     status =  subp.returncode
     if status == 0:
         return True
@@ -119,20 +118,20 @@ def holdWait():
 class Listener(BaseHTTPRequestHandler):
 
     def getData(self):
-      
+
         datas = self.rfile.read(int(self.headers['content-length']))
         datas = urllib.unquote(datas).decode("utf-8", 'ignore')
         content = str(datas) +"\r\n"
 
-    
-       
-        enc="UTF-8"  
-        content = content.encode(enc)          
-        f = io.BytesIO()  
-        f.write(content)  
-        f.seek(0)  
-        self.send_response(200)  
-        self.send_header("Content-type", "text/html; charset=%s" % enc)  
-        self.send_header("Content-Length", str(len(content)))  
-        self.end_headers()  
-        shutil.copyfileobj(f,self.wfile) 
+
+        enc="UTF-8"
+        content = content.encode(enc)
+        f = io.BytesIO()
+        f.write(content)
+        f.seek(0)
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=%s" % enc)
+        self.send_header("Content-Length", str(len(content)))
+        self.end_headers()
+        shutil.copyfileobj(f,self.wfile)
+
