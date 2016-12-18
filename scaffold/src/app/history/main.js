9462b018d314cc027c699a6266bf41582d4ac8bb
viviXie <wwxie830@163.cm>
2016-12-18 17:07:25 +0800

    containerlogs

--- scaffold/src/app/history/main.js
+++ scaffold/src/app/history/main.js
@@ -19,6 +19,7 @@ import * as historyDataService from "./historyData";
 import { setPath } from "../relation/setPath";
 import { notify } from "../common/notify";
 import { getActionHistory } from "./actionHistory";
+import { getContainerLogs } from "./actionHistory";
 import { getLineHistory } from "./lineHistory";
 import { getHistoryList } from "./historyList";
 // import * as initButton from "../workflow/initButton";
@@ -158,6 +159,7 @@ export function getSequenceDetail(selected_history) {
         } else if( data.define.stageList.length > 0 && isAction){
             initSequenceView(selected_history);
             getActionHistory(historyAbout.workflowName,historyAbout.versionName,historyAbout.sequence,historyAbout.stageName,historyAbout.actionName);
+            getContainerLogs(historyAbout.workflowName,historyAbout.versionName,historyAbout.sequence,historyAbout.stageName,historyAbout.actionName);
         }
 
     });
@@ -827,6 +829,7 @@ function initSequenceActionByStage() {
                 .on("click", function(ad, ai) {
                     // workflowName,versionName,workflowRunSequence,stageName,actionName
                     getActionHistory(historyAbout.workflowName,historyAbout.versionName,historyAbout.sequence, d.setupData.name, ad.setupData.name);
+                    getContainerLogs(historyAbout.workflowName,historyAbout.versionName,historyAbout.sequence, d.setupData.name, ad.setupData.name);
                     if (ad.status == 1 || ad.status == 0) {
                         historyChangeCurrentElement(constant.currentSelectedItem);
                         constant.setCurrentSelectedItem({ "data": ad, "parentData": d, "type": "action", "status": ad.status });
