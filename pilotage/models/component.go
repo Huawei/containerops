ea056a9c328e014f2b2e3608a22611e0b9a3ce1b
soso <sosozhuang@163.com>
2016-12-29 19:03:22 +0800

    Update component.go

--- pilotage/models/component.go
+++ pilotage/models/component.go
@@ -33,31 +33,29 @@ const (
 
 //Component is customized container(docker or rkt) for executing DevOps tasks.
 type Component struct {
-	ID             int64      `json:"id" gorm:"primary_key"`
-	Namespace      string     `json:"namespace" sql:"not null;type:varchar(255)"` //User or organization.
-	Version        string     `json:"version" sql:"null;type:text"`               // component version for display
-	VersionCode    int64      `json:"versionCode" sql:"null;type:bigint"`         // component version code system set
-	Component      string     `json:"component" sql:"not null;type:varchar(255)"` //Component name for query.
-	Type           int64      `json:"type" sql:"not null;default:0"`              //Container type: docker or rkt.
-	Title          string     `json:"title" sql:"null;type:varchar(255)"`         //Component name for display.
-	Gravatar       string     `json:"gravatar" sql:"null;type:text"`              //Logo.
-	Description    string     `json:"description" sql:"null;type:text"`           //Description with markdown style.
-	Endpoint       string     `json:"endpoint" sql:"null;type:text"`              //Contaienr location like: `dockyard.sh/genedna/cloudnativeday:1.0`.
-	Source         string     `json:"source" sql:"not null;type:text"`            //Component source location like: `git@github.com/containerops/components`.
-	Environment    string     `json:"environment" sql:"null;type:text"`           //Environment parameters.
-	Tag            string     `json:"tag" sql:"null;type:varchar(255)"`           //Tag for version.
-	VolumeLocation string     `json:"volume_location" sql:"null;type:text"`       //Volume path in the container.
-	VolumeData     string     `json:"volume_data" sql:"null;type:text"`           //Volume data source.
-	Makefile       string     `json:"makefile" sql:"null;type:text"`              //Like Dockerfile or acbuild script.
-	Kubernetes     string     `json:"kubernetes" sql:"null;type:text"`            //Kubernetes execute script.
-	Swarm          string     `json:"swarm" sql:"null;type:text"`                 //Docker Swarm execute script.
-	Input          string     `json:"input" sql:"null;type:text"`                 //component input
-	Output         string     `json:"output" sql:"null;type:text"`                //component output
-	Timeout        int64      `json:"timeout"`                                    //
-	Manifest       string     `json:"manifest" sql:"null;type:longtext"`          //
-	CreatedAt      time.Time  `json:"created" sql:""`                             //
-	UpdatedAt      time.Time  `json:"updated" sql:""`                             //
-	DeletedAt      *time.Time `json:"deleted" sql:"index"`                        //
+	BaseIDField
+	Namespace   string     `json:"namespace" sql:"not null;type:varchar(255)"` //User or organization.
+	Version     string     `json:"version" sql:"null;type:text"`               // component version for display
+	VersionCode int64      `json:"versionCode" sql:"null;type:bigint"`         // component version code system set
+	Name        string     `json:"name" sql:"not null;type:varchar(255)"`      //Component name for query.
+	Type        int64      `json:"type" sql:"not null;default:0"`              //Container type: docker or rkt.
+	Title       string     `json:"title" sql:"null;type:varchar(255)"`         //Component name for display.
+	Gravatar    string     `json:"gravatar" sql:"null;type:text"`              //Logo.
+	Description string     `json:"description" sql:"null;type:text"`           //Description with markdown style.
+	Endpoint       string     `json:"endpoint" sql:"null;type:text"`           //Contaienr location like: `dockyard.sh/genedna/cloudnativeday:1.0`.
+	Source         string     `json:"source" sql:"not null;type:text"`         //Component source location like: `git@github.com/containerops/components`.
+	Environment    string     `json:"environment" sql:"null;type:text"`        //Environment parameters.
+	Tag            string     `json:"tag" sql:"null;type:varchar(255)"`        //Tag for version.
+	VolumeLocation string     `json:"volume_location" sql:"null;type:text"`    //Volume path in the container.
+	VolumeData     string     `json:"volume_data" sql:"null;type:text"`        //Volume data source.
+	Makefile       string     `json:"makefile" sql:"null;type:text"`           //Like Dockerfile or acbuild script.
+	Kubernetes     string     `json:"kubernetes" sql:"null;type:text"`         //Kubernetes execute script.
+	Swarm          string     `json:"swarm" sql:"null;type:text"`              //Docker Swarm execute script.
+	Input          string     `json:"input" sql:"null;type:text"`              //component input
+	Output         string     `json:"output" sql:"null;type:text"`             //component output
+	Timeout        int64      `json:"timeout"`                                 //
+	Manifest       string     `json:"manifest" sql:"null;type:longtext"`       //
+	BaseModel
 }
 
 //TableName is return the table name of Component in MySQL database.
@@ -67,4 +65,15 @@ func (c *Component) TableName() string {
 
 func (c *Component) GetComponent() *gorm.DB {
 	return db.Model(&Component{})
+}
+
+func (c *Component) Create() error {
+	return db.Create(c).Error
+}
+
+func SelectComponentFromID(id uint64) (component *Component, err error) {
+	var condition *Component
+	condition.ID = id
+	err = db.Where(condition).First(component).Error
+	return
 }
