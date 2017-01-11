8abc53b84e65c36a484228ec192b50d4a2299cda
ChengXi <xcheng@linkernetworks.com>
2017-01-11 14:47:01 +0800

    change login

--- scaffold-new/src/js/services/login/main.js
+++ scaffold-new/src/js/services/login/main.js
@@ -1,46 +1,7 @@
-define(['login'], function (app) {
-    'use strict';
-     app.provide.factory('LoginService', ['$http','$q',function ($http,$q) {
-	    	return {
-                doLogin : function(user){
-					var deferred = $q.defer();
-					var url = "/user/login";		
-					var request = {
-						"url": url,
-						"dataType": "json",
-						"method": "POST",
-						"data":angular.toJson(user)
-					}
-						
-					$http(request).success(function(data){
-						deferred.resolve(data);
-					}).error(function(error){
-						deferred.reject(error);
-					});
-					return deferred.promise;
-					
-			    },
-
-			    doSignup : function(user){
-					var deferred = $q.defer();
-					var url = "/user/signup";		
-					var request = {
-						"url": url,
-						"dataType": "json",
-						"method": "POST",
-						"data":angular.toJson(user)
-					}
-						
-					$http(request).success(function(data){
-						deferred.resolve(data);
-					}).error(function(error){
-						deferred.reject(error);
-					});
-					return deferred.promise;
-					
-			    }
-			  
-	    	}
-	    	
-     }]);
-});
+function loginService(http,q){
+	return {
+		
+	}
+}
+   
+login.factory('loginService', ['$http','$q',loginService]);
