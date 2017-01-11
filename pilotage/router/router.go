582baa3c1ee7ddc870f80eb11b26a13cafde4124
Marx Guo <go.morefork@gmail.com>
2017-01-11 11:14:14 +0800

    modify pilotage to pingcap-version
    Signed-off-by: Marx Guo <go.morefork@gmail.com>

--- pilotage/router/router.go
+++ pilotage/router/router.go
@@ -17,45 +17,32 @@ limitations under the License.
 package router
 
 import (
-	"github.com/Huawei/containerops/pilotage/handler"
-	"github.com/Huawei/containerops/pilotage/middleware"
 	"github.com/go-macaron/binding"
 	"gopkg.in/macaron.v1"
+
+	"github.com/Huawei/containerops/pilotage/handler"
 )
 
-//SetRouters is pilotage router's definition function.
+//SetRouters is pilotage router's definition fucntion.
 func SetRouters(m *macaron.Macaron) {
 	m.Group("/v2", func() {
 		m.Get("/", handler.IndexV1Handler)
 
-		m.Group("/events", func() {
-			m.Post("/", handler.CreateEvent)
-		})
-
-		m.Group("/components", func() {
-			m.Get("/", handler.ListComponents)
-
-			m.Post("/", handler.CreateComponent)
-			m.Get("/:component", handler.GetComponent)
-			m.Put("/:component", handler.UpdateComponent)
-			m.Delete("/:component", handler.DeleteComponent)
-
-			m.Get("/:component/debug", handler.DebugComponentJson(), handler.DebugComponentLog)
-			//m.Get("/ws/debug",
-			//	sockets.JSON(handler.DebugComponentMessage{}),
-			//	func(
-			//	receiver <-chan *handler.DebugComponentMessage,
-			//	sender chan<- *handler.DebugComponentMessage,
-			//	done <-chan bool,
-			//	disconnect chan<- int,
-			//	errChan <-chan error) {})
-		})
-
-		m.Group("/images", func() {
-			m.Post("/build", handler.BuildImage)
-		})
-
 		m.Group("/:namespace", func() {
+
+			m.Group("/component", func() {
+				m.Get("/list", handler.GetComponentListV1Handler)
+				m.Post("/", handler.PostComponentV1Handler)
+
+				m.Get("/:component", handler.GetComponentV1Handler)
+				m.Put("/:component", handler.PutComponentV1Handler)
+				m.Delete("/:component", handler.DeleteComponentv1Handler)
+
+				m.Post("/:component/event", handler.PostEventV1Handler)
+				m.Get("/:component/event/:event", handler.GetEventV1Handler)
+				m.Put("/:component/event/:event", handler.PutEventV1Handler)
+				m.Delete("/:component/event/:event", handler.DeleteEventV1Handler)
+			})
 
 			m.Group("/service", func() {
 				m.Get("/list", handler.GetServiceDefinitionListV1Handler)
@@ -98,8 +85,8 @@ func SetRouters(m *macaron.Macaron) {
 						m.Post("/exec/:workflow", handler.ExecuteWorkflowV1Handler)
 
 						m.Group("/runtime", func() {
-							//m.Post("/event/:workflow/register", handler.PostActionRegisterV1Handler)
-							//m.Post("/event/:workflow/:event", handler.PostActionEventV1Handler)
+							m.Post("/event/:workflow/register", handler.PostActionRegisterV1Handler)
+							m.Post("/event/:workflow/:event", handler.PostActionEventV1Handler)
 
 							m.Post("/var/:workflow", handler.PostActionSetVarV1Handler)
 
@@ -118,33 +105,7 @@ func SetRouters(m *macaron.Macaron) {
 							m.Get("/:workflow/:version/:sequence/stage/:stage/action/:action/console/log", handler.GetActionConsoleLogV1Handler)
 							m.Get("/:workflow/:version/:sequence/:relation", handler.GetSequenceLineHistoryV1Handler)
 						})
-
-						m.Group("/workflows", func() {
-							m.Get("/", handler.ListWorkflowsV1)
-							m.Post("/", func() {})
-
-							m.Get("/:workflowID", func() {})
-							m.Put("/:workflowID", func() {})
-							m.Delete("/:workflowID", func() {})
-						})
-
-						m.Group("/event", func() {
-							m.Post("/", func() {})
-						})
-
-						m.Group("/webhook", func() {
-							m.Post("/:workflowID", func() {})
-						})
-
-						m.Group("/histories", func() {
-							m.Get("/", func() {})
-
-							m.Get("/workflows/:workflowID", func() {})
-							m.Get("/workflows/:workflowID/sequenceID", func() {})
-							m.Get("/stages/:stageID", func() {})
-							m.Get("/actions/:actionID", func() {})
-						})
-					}, middleware.PreChecker)
+					})
 				})
 			})
 		})
