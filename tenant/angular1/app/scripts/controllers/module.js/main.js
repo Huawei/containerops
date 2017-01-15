e64008775c9f2f2ed30e82cf74ade58a1b3ab9a8
wuxiaoxiao <small145900@163.com>
2017-01-15 22:27:02 +0800

    add module page & service

--- tenant/angular1/app/scripts/controllers/module.js/main.js
+++ tenant/angular1/app/scripts/controllers/module.js/main.js
@@ -1,4 +1,74 @@
-auth.controller('ModuleController', ['$scope', '$location', function($scope, $location) {
+auth.controller('ModuleController', ['$scope', '$location', '$state', 'moduleService', function($scope, $location, $state, moduleService) {
+	
+	$scope.moduleList = [];
 
+	$scope.create = function(){
+  	$state.go("project.create");
+  }
+
+	$scope.params = {
+		startNum: $scope.moduleList.length,
+		endNum: $scope.moduleList.length+10
+	}
+
+	$scope.getList = function(params){
+		moduleService.getList(params)
+			.then(function(data){
+				// $scope.moduleList = data;
+				$scope.moduleList = [
+					{
+						"id":100,
+						"name":"p1",
+						"desc":"p1"
+					},
+					{
+						"id":101,
+						"name":"p2",
+						"desc":"p2"
+					}
+				]
+			}, function(err){
+				console.log('获取module list err:',err);
+				$scope.moduleList = [
+					{
+						"id":100,
+						"name":"p1",
+						"desc":"p1"
+					},
+					{
+						"id":101,
+						"name":"p2",
+						"desc":"p2"
+					}
+				]
+			})	
+	};
+
+	$scope.getList($scope.params);
+
+}])
+.controller('ModuleCreateController', ['$scope', '$location','$state', 'moduleService', function($scope, $location, $state, moduleService) {
+  $scope.baseInfo = {
+		name: '',
+		desc: '',
+		visible: 'public'
+	};
+
+
+	$scope.saveBaseInfo = function(){
+		console.log($scope.baseInfo)
+		var baseInfo = $scope.baseInfo;
+		if(baseInfo.name){
+			moduleService.saveBaseInfo(baseInfo)
+				.then(function(data){
+					console.log('保存成功')
+				},function(err){
+					console.log('保存失败：',err)
+				})
+		}else{
+			console.log('请填写用户名')
+		}
+	}
 
 }]);
+
