b2c281648d77f49628cd51df89a11fd8fbdbf40e
ZhengYongJing <yongjingzheng1983@gmail.com>
2017-01-18 23:43:20 +0800

    add create org function

--- tenant/angular1/app/scripts/controllers/team/main.js
+++ tenant/angular1/app/scripts/controllers/team/main.js
@@ -1,92 +1,98 @@
-auth.controller('TeamController', ['$scope', '$location','$state', 'TeamService','OrganizationService',function($scope, $location,$state,TeamService,OrganizationService) {
+auth.controller('TeamController', ['$scope', '$location', '$state', 'TeamService', 'OrganizationService', function($scope, $location, $state, TeamService, OrganizationService) {
 
-     $scope.get = function() {
-        OrganizationService.get().then(function(data){
-            	 $scope.orgs = data;
-            	 $scope.selectedOrg = $scope.orgs[0];
-	             TeamService.get(orgid).then(function(data) {
-	                $scope.teams = [{"name":"team1","desc":"desc1"}];
-	            },
-	            function(errMsg) {
-	                console.log("error")
-	                $scope.teams = [{"name":"team1","desc":"desc1"}];
-	            });
-        },function(errMsg){
-            $scope.orgs = [{"name":"org1","desc":"desc1"}];
-            $scope.selectedOrg = $scope.orgs[0];
-            TeamService.get($scope.selectedOrg.name).then(function(data) {
-	                $scope.teams = [{"name":"team1","desc":"desc1"}];
-	            },
-	            function(errMsg) {
-	                console.log("error")
-	                $scope.teams = [{"name":"team1","desc":"desc1"}];
-	            });
-        })
-       
-    };
-    $scope.create = function(){
-    	$state.go("team.create");
-    }
-    $scope.get();
+        $scope.get = function() {
+            OrganizationService.get().then(function(data) {
+                $scope.orgs = data;
+                $scope.selectedOrg = $scope.orgs[0];
+                TeamService.get(orgid).then(function(data) {
+                        $scope.teams = [{ "name": "team1", "desc": "desc1" }];
+                    },
+                    function(errMsg) {
+                        console.log("error")
+                        $scope.teams = [{ "name": "team1", "desc": "desc1" }];
+                    });
+            }, function(errMsg) {
+                $scope.orgs = [{ "name": "org1", "desc": "desc1" }];
+                $scope.selectedOrg = $scope.orgs[0];
+                TeamService.get($scope.selectedOrg.name).then(function(data) {
+                        $scope.teams = [{ "name": "team1", "desc": "desc1" }];
+                    },
+                    function(errMsg) {
+                        console.log("error")
+                        $scope.teams = [{ "name": "team1", "desc": "desc1" }];
+                    });
+            })
 
-}])
-.controller('TeamCreateController', ['$scope', '$location','$state', 'TeamService','OrganizationService',function($scope, $location,$state,TeamService,OrganizationService) {
-     OrganizationService.get().then(function(data){
-        $scope.orgs = [{"name":"org1","desc":"desc1"}];
-        $scope.selectedOrg = $scope.orgs.length > 0 ? $scope.orgs[0] : {"name":"","desc":""};
-     },
-     function(errMsg) {
-        $scope.orgs = [{"name":"org1","desc":"desc1"}];
-        $scope.selectedOrg = $scope.orgs.length > 0 ? $scope.orgs[0] : {"name":"","desc":""};
-     })
-     $scope.team = {name:"",description:"",type:"public"};
-     $scope.step = "basicInfo";
-     $scope.members = [];
-     $scope.back = function(){
-     	$state.go("team");
-     }
-     $scope.save = function(){
-         TeamService.save($scope.team).then(function(data){
-            console.log("create success")
-            $scope.team = {};
-         },
-         function(errMsg){
-         	console.log("create error")
-         	$scope.team.id = 1;
-         	$scope.step = "inviteMember";
-         })
-     };
-     $scope.addMember = function(){
-        
-         TeamService.addMember($scope.team.id, $scope.newMember).then(function(data){
-             TeamService.getMembers($scope.team.id).then(function(data){
-                $scope.members.push($scope.newMember);
-             },
-             function(errMsg){
-                $scope.members.push($scope.newMember);
-             })
-         },
-         function(errMsg){
-              $scope.members.push($scope.newMember);
-              $scope.newMember="";
-         })
-     };
-     $scope.removeMember = function(member){
-        TeamService.removeMember($scope.team.id, member).then(function(data){
-             TeamService.getMembers($scope.team.id).then(function(data){
-                $scope.members = _.without($scope.members, member);
-             },
-             function(errMsg){
-                $scope.members = _.without($scope.members, member);
-             })
-         },
-         function(errMsg){
-              $scope.members = _.without($scope.members, member);
-         })
-     };
-     $scope.gotoCreate = function(){
-        $scope.step="basicInfo";
-         $scope.team = {name:"",description:"",type:"public"};
-     }
+        };
+        $scope.create = function() {
+            $state.go("team.create");
+        }
+        $scope.get();
 
-}])
+    }])
+    .controller('TeamCreateController', ['$scope', '$location', '$state', 'TeamService', 'OrganizationService', function($scope, $location, $state, TeamService, OrganizationService) {
+        OrganizationService.get().then(function(data) {
+                $scope.orgs = [{ "name": "org1", "desc": "desc1" }];
+                $scope.selectedOrg = $scope.orgs.length > 0 ? $scope.orgs[0] : { "name": "", "desc": "" };
+            },
+            function(errMsg) {
+                $scope.orgs = [{ "name": "org1", "desc": "desc1" }];
+                $scope.selectedOrg = $scope.orgs.length > 0 ? $scope.orgs[0] : { "name": "", "desc": "" };
+            })
+        $scope.team = { name: "", description: "", type: "public" };
+        $scope.step = "basicInfo";
+        $scope.members = [];
+        // $scope.newMemberValidated = true;
+        $scope.back = function() {
+            $state.go("team");
+        }
+        $scope.save = function() {
+            TeamService.save($scope.team).then(function(data) {
+                    console.log("create success")
+                    $scope.team = {};
+                },
+                function(errMsg) {
+                    console.log("create error")
+                    $scope.team.id = 1;
+                    $scope.step = "inviteMember";
+                })
+        };
+        $scope.saveMember = function() {
+            if (!_.isEmpty($scope.newMember)) {
+                // $scope.newMemberValidated = false;
+            // } else {
+                TeamService.saveMember($scope.team.id, $scope.newMember).then(function(data) {
+                        TeamService.getMembers($scope.team.id).then(function(data) {
+                                $scope.members.push($scope.newMember);
+                            },
+                            function(errMsg) {
+                                $scope.members.push($scope.newMember);
+                            })
+                    },
+                    function(errMsg) {
+                        $scope.members.push($scope.newMember);
+                        $scope.newMember = "";
+                    })
+            }
+
+        };
+
+        $scope.removeMember = function(member) {
+            TeamService.removeMember($scope.team.id, member).then(function(data) {
+                    TeamService.getMembers($scope.team.id).then(function(data) {
+                            $scope.members = _.without($scope.members, member);
+                        },
+                        function(errMsg) {
+                            $scope.members = _.without($scope.members, member);
+                        })
+                },
+                function(errMsg) {
+                    $scope.members = _.without($scope.members, member);
+                })
+        };
+        $scope.gotoCreate = function() {
+            $scope.step = "basicInfo";
+            $scope.team = { name: "", description: "", type: "public" };
+        }
+
+    }])
