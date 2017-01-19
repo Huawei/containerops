f9d4796a48a850bbeb381f2aa510d16fb343512a
ZhengYongJing <yongjingzheng1983#gmail.com>
2017-01-19 19:09:48 +0800

    improve create organization function

--- tenant/angular1/app/scripts/controllers/team/main.js
+++ tenant/angular1/app/scripts/controllers/team/main.js
@@ -1,33 +1,34 @@
 auth.controller('TeamController', ['$scope', '$location', '$state', 'TeamService', 'OrganizationService', function($scope, $location, $state, TeamService, OrganizationService) {
-
-        $scope.get = function() {
+        $scope.selected = {orgId:""};
+        $scope.init = function() {
             OrganizationService.get().then(function(data) {
-                $scope.orgs = data;
-                $scope.selectedOrg = $scope.orgs[0];
-                TeamService.get(orgid).then(function(data) {
-                        $scope.teams = [{ "name": "team1", "desc": "desc1" }];
-                    },
-                    function(errMsg) {
-                        console.log("error")
-                        $scope.teams = [{ "name": "team1", "desc": "desc1" }];
-                    });
+                $scope.orgs = [{ "name": "org1", "desc": "desc1","id":"1"},{ "name": "org2", "desc": "desc2","id":"2" }];
+                $scope.setSelectedOrg($scope.orgs[0].id);
+                $scope.get();
             }, function(errMsg) {
-                $scope.orgs = [{ "name": "org1", "desc": "desc1" }];
-                $scope.selectedOrg = $scope.orgs[0];
-                TeamService.get($scope.selectedOrg.name).then(function(data) {
-                        $scope.teams = [{ "name": "team1", "desc": "desc1" }];
-                    },
-                    function(errMsg) {
-                        console.log("error")
-                        $scope.teams = [{ "name": "team1", "desc": "desc1" }];
-                    });
+                $scope.orgs = [{ "name": "org1", "desc": "desc1","id":"1"},{ "name": "org2", "desc": "desc2","id":"2"}];
+                $scope.setSelectedOrg($scope.orgs[0].id);
+                $scope.get();
             })
 
+        };
+        $scope.get = function() {
+            TeamService.get($scope.selected.orgId).then(function(data) {
+                    $scope.teams = [{ "name": "team1", "desc": "desc1" }];
+                },
+                function(errMsg) {
+                    $scope.teams = [{ "name": "team1", "desc": "desc1" }];
+                    // console.log("from controller")
+                    // console.log($scope.selected.orgId)
+                });
         };
         $scope.create = function() {
             $state.go("team.create");
         }
-        $scope.get();
+        $scope.setSelectedOrg = function(item){
+            $scope.selected.orgId = item;
+        }
+        $scope.init();
 
     }])
     .controller('TeamCreateController', ['$scope', '$location', '$state', 'TeamService', 'OrganizationService', function($scope, $location, $state, TeamService, OrganizationService) {
@@ -42,6 +43,7 @@ auth.controller('TeamController', ['$scope', '$location', '$state', 'TeamService
         $scope.team = { name: "", description: "", type: "public" };
         $scope.step = "basicInfo";
         $scope.members = [];
+        $scope.newMember = "";
         // $scope.newMemberValidated = true;
         $scope.back = function() {
             $state.go("team");
@@ -60,7 +62,7 @@ auth.controller('TeamController', ['$scope', '$location', '$state', 'TeamService
         $scope.saveMember = function() {
             if (!_.isEmpty($scope.newMember)) {
                 // $scope.newMemberValidated = false;
-            // } else {
+                // } else {
                 TeamService.saveMember($scope.team.id, $scope.newMember).then(function(data) {
                         TeamService.getMembers($scope.team.id).then(function(data) {
                                 $scope.members.push($scope.newMember);
