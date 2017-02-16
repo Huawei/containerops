4b79b1cd736b1f70652a081462ff6b3b306590bb
ZhengYongJing <yongjingzheng1983@gmail.com>
2017-02-16 19:18:07 +0800

    add workflow js and templates

--- joints/src/js/controllers/workflow/main.js
+++ joints/src/js/controllers/workflow/main.js
@@ -1,6 +1,13 @@
 define(["app"], function(app) {
-    app.controllerProvider.register('WorkflowController', ['$scope', '$location','notifyService', function($scope, $location,notifyService) {
-        $scope.workflow = "workflow controllerProvider";
-        // notifyService.notify("zyjtest","success");
+    app.controllerProvider.register('WorkflowController', ['$scope', '$state', 'notifyService', function($scope, $state, notifyService) {
+        $scope.workflows = [{"name":"workflow1","versions":["v1.0","v1.1"]}]
+        $scope.showCreateWorkflow = function(){
+        	$state.go("workflow.create");
+        };
+        $scope.showMoreVersion = function(verionname){
+        	notifyService.notify("no more workflow","info")
+        };
+
+
     }]);
 })
