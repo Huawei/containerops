95fa993da88d42795e0466e5de9b3952e7347674
Cheng Xi <xcheng19820201@gmail.com>
2017-02-17 15:22:18 +0800

    style changes
    Signed-off-by: Cheng Xi <xcheng19820201@gmail.com>

--- joints/src/js/controllers/workflow/main.js
+++ joints/src/js/controllers/workflow/main.js
@@ -1,6 +1,18 @@
 define(["app"], function(app) {
     app.controllerProvider.register('WorkflowController', ['$scope', '$state', 'notifyService', function($scope, $state, notifyService) {
-        $scope.workflows = [{"name":"workflow1","versions":["v1.0","v1.1"]}]
+        $scope.workflows = [{
+        		"name":"workflow1",
+        		"versions":[
+        			{
+        				"id" : 1,
+        				"version" : "v1.0"
+        			},
+        			{
+        				"id" : 2,
+        				"version" : "v1.0"
+        			}
+        		]
+        }]
         $scope.showCreateWorkflow = function(){
         	$state.go("workflow.create");
         };
