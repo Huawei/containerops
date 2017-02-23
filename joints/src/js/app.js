04aebb00a9f6505461a70790f649265dca6e7825
Cheng Xi <xcheng19820201@gmail.com>
2017-02-23 20:11:38 +0800

    component changes based on latest request, partial codes
    Signed-off-by: Cheng Xi <xcheng19820201@gmail.com>

--- joints/src/js/app.js
+++ joints/src/js/app.js
@@ -1,7 +1,8 @@
 define([
 	'angular',
-	'services/index'
+	'services/index',
+	'directives/index'
 	], function(angular) {
-	return angular.module("DevOps", ['ui.router', 'ui.bootstrap','ngWebSocket', 'app.services'
+	return angular.module("DevOps", ['ui.router', 'ui.bootstrap','ngWebSocket', 'app.services','app.directives'
         ]);
 })
