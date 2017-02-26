eb4fd8a278e82cfa875fd38b9fdd773dffd1217f
wuxiaoxiao <small145900@163.com>
2017-02-27 00:26:34 +0800

    data binding for action

--- joints/src/js/controllers/workflow/element.js
+++ joints/src/js/controllers/workflow/element.js
@@ -1,12 +1,12 @@
 define(["app","services/diagram/main"], function(app) {
     app.controllerProvider.register('WorkflowElementController', ['$scope', '$state', 'notifyService', 'diagramService', function($scope, $state, notifyService, diagramService) {
         $scope.workflows = [{"name":"workflow1","versions":["v1.0","v1.1"]}]
-        $scope.showCreateWorkflow = function(){
-        	$state.go("workflow.detail");
-        };
-        $scope.showMoreVersion = function(verionname){
-        	notifyService.notify("no more workflow","info")
-        };
+        // $scope.showCreateWorkflow = function(){
+        // 	$state.go("workflow.detail");
+        // };
+        // $scope.showMoreVersion = function(verionname){
+        // 	notifyService.notify("no more workflow","info")
+        // };
 
         $scope.workflowData = diagramService.workflowData;
         $scope.currentStageInfo = $scope.workflowData[diagramService.currentStageIndex];
