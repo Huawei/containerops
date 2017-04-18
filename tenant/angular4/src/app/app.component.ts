df64f4b4ed54f4f9dd4d8f0c72693a787265ce39
Meng Fanliang <mengfanliang@huawei.com>
2017-04-18 17:38:23 +0800

    update tenant/angular4

--- tenant/angular4/src/app/app.component.ts
+++ tenant/angular4/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  title = 'app works!';
+}
