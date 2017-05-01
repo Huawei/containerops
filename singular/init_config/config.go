27c54f9fe3b0e9f1da33ed0a5ff075b2581e7313
Dian Li <lidian1@huawei.com>
2017-05-01 19:21:49 +0800

    update for remote deploying k8s master node

---
+++ singular/init_config/config.go
@@ -0,0 +1,16 @@
+package init_config
+
+// import (
+// 	"fmt"
+// 	)
+
+var MasterIP string = "45.55.14.171"
+var NodeIP string = "45.55.14.171"
+
+var User string = "root"
+
+var TSpet string = "6f2671de0d70ee5048379d16c0d0405df4a720ced263ffb35f67aded4834f330"
+
+// func test() {
+// 	fmt.Println("cccc")
+// }
