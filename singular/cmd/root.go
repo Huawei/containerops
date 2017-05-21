c97bcaecbbe32a226913c39ede7c40035ff3a688
Dian Li <lidian1@huawei.com>
2017-05-22 01:55:35 +0800

    update cmd login and readme

--- singular/cmd/root.go
+++ singular/cmd/root.go
@@ -20,19 +20,19 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/Huawei/containerops/singular/vm"
 	cobra "github.com/spf13/cobra"
 )
 
-// RootCmd is root cmd of dockyard.
 var RootCmd = &cobra.Command{
 	Use:   "singular",
-	Short: "The deployment and operations tools.",
+	Short: "The kubernetes deployment and operations tools",
 	Long:  ``,
-	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
-		fmt.Println("111")
+	Run:   rootion,
+}
 
-	},
+func rootion(cmd *cobra.Command, args []string) {
+	fmt.Println("Singular version 0.1, build 5604cbe")
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
@@ -44,26 +44,147 @@ func Execute() {
 	}
 }
 
-// init()
+var mSize int
+var region string
+var count int
+
 func init() {
-	RootCmd.AddCommand(versionCmd)
-	//RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
-	versionCmd.Flags().StringVarP(&address, "address", "a", "0.0.0.0", "http or https listen address.")
-	versionCmd.Flags().Int64VarP(&port, "port", "p", 80, "the port of http.")
+	//RootCmd.AddCommand(versionCmd)
+	RootCmd.Flags().StringVarP(&address, "version", "v", "", "Show the Singular version information")
+
+	//ConfigCmd
+	RootCmd.AddCommand(ConfigCmd)
+
+	ConfigCmd.Flags().IntVarP(&mSize, "mSize", "m", 512, "Node memory Size")
+	ConfigCmd.Flags().StringVarP(&region, "region", "r", "sfo", "Cluster's localization of the region")
+	ConfigCmd.Flags().StringVarP(&region, "system", "s", "ubuntu-17-04-x64", "Virtual machine system version")
+
+	//createCmd
+	RootCmd.AddCommand(createCmd)
+	createCmd.Flags().IntVarP(&count, "count", "c", 3, "Number of nodes in cluster")
+	createCmd.AddCommand(pullCmd)
+
+	RootCmd.AddCommand(cerkeyCmd)
+
+	RootCmd.AddCommand(deployCmd)
+
+	// versionCmd.Flags().StringVarP(&address, "address", "a", "0.0.0.0", "http or https listen address.")
+	// versionCmd.Flags().Int64VarP(&port, "port", "p", 80, "the port of http.")
+	// versionCmd.Flags().StringVarP(&address, "a", "v", "", "Source directory to read from")
+
 }
 
 var address string
 var port int64
 
-var versionCmd = &cobra.Command{
-	Use:   "install",
-	Short: "Start to install kubenetes cluster automatically by the configuration file",
-	Long:  `Start to install kubenetes cluster automatically by the configuration file`,
-	Run:   startInstall,
+// var versionCmd = &cobra.Command{
+// 	Use:   "version",
+// 	Short: "",
+// 	Long:  ``,
+// 	Run:   version,
+// }
+
+// func version(cmd *cobra.Command, args []string) {
+// 	fmt.Println("Singular version 0.1, build 5604cbe  " + address)
+// }
+
+var ConfigCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Configure your nodes of kubernetes cluster",
+	Long:  `Configure your nodes of kubernetes cluster`,
+	Run:   config,
 }
 
-func startInstall(cmd *cobra.Command, args []string) {
-	fmt.Println("Start to install kubenetes cluster automatically by the configuration file" + address)
+func config(cmd *cobra.Command, args []string) {
+	fmt.Println("" + address)
 	// fmt.Println("Hugo Static Site Generator v0.9 -- HEAD  " + strings.Join(port, ""))
 
+}
+
+var installCmd = &cobra.Command{
+	Use:   "install",
+	Short: "",
+	Long:  ``,
+	Run:   Install,
+}
+
+func Install(cmd *cobra.Command, args []string) {
+	fmt.Println("" + address)
+}
+
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "",
+	Long:  ``,
+	Run:   create,
+}
+
+func create(cmd *cobra.Command, args []string) {
+	//vm.CreateNewVM("lidian-unbantu-wk-master")
+
+	vm.CreateVMs()
+}
+
+var pullCmd = &cobra.Command{
+	Use:   "create",
+	Short: "",
+	Long:  ``,
+	Run:   pull,
+}
+
+func pull(cmd *cobra.Command, args []string) {
+	fmt.Println("" + address)
+}
+
+var deployCmd = &cobra.Command{
+	Use:   "deploy",
+	Short: "",
+	Long:  ``,
+	Run:   deploy,
+}
+
+func deploy(cmd *cobra.Command, args []string) {
+	fmt.Println("" + address)
+
+	//get iplist while
+	// download.Download_main()
+	// deploy.DeployNodes()
+
+}
+
+var apikeyCmd = &cobra.Command{
+	Use:   "apikey",
+	Short: "",
+	Long:  ``,
+	Run:   apikey,
+}
+
+func apikey(cmd *cobra.Command, args []string) {
+
+	fmt.Println("[singular] API Server key is ready.")
+
+}
+
+var cerpathCmd = &cobra.Command{
+	Use:   "cerpath",
+	Short: "",
+	Long:  ``,
+	Run:   cerpath,
+}
+
+func cerpath(cmd *cobra.Command, args []string) {
+	fmt.Println("" + address)
+}
+
+var cerkeyCmd = &cobra.Command{
+	Use:   "cerkey",
+	Short: "",
+	Long:  ``,
+	Run:   cerkey,
+}
+
+func cerkey(cmd *cobra.Command, args []string) {
+	fmt.Println("[singular] Generated Certificate Authority key and certificate.")
+	fmt.Println("[singular] Created keys and certificates in \"/usr/singular/\"")
+	fmt.Println(args)
 }
--- singular/download/download-master.go
+++ singular/download/download-master.go
@@ -19,7 +19,6 @@ package download
 import (
 	"fmt"
 
-	cmd "github.com/Huawei/containerops/singular/cmd"
 	"github.com/Huawei/containerops/singular/init_config"
 )
 
@@ -27,10 +26,10 @@ func Master_Download(ip string) {
 
 	var fileslist = init_config.Get_files()
 	for key, url := range fileslist {
-		cmd.ExecCMDparams("wget", []string{"-P", "/tmp/", "-c", url})
+		ExecCMDparams("wget", []string{"-P", "/tmp/", "-c", url})
 		fmt.Printf("%s\n\n", key)
 	}
 	//scp -r ./config/. root@138.68.14.193:/tmp/
-	cmd.LocalExecCMDparams("scp", []string{"-r", "./config/.", init_config.User + "@" + ip + ":" + "/tmp/config/"})
+	LocalExecCMDparams("scp", []string{"-r", "./config/.", init_config.User + "@" + ip + ":" + "/tmp/config/"})
 
 }
