f5fffc4726d234b5506d54813cf672b80ddc9ca3
DeanLee <mail@imlidian.com>
2017-05-22 01:55:35 +0800

    update cmd login and readme

--- singular/main.go
+++ singular/main.go
@@ -18,19 +18,10 @@ package main
 
 import (
 	"github.com/Huawei/containerops/singular/cmd"
-	"github.com/Huawei/containerops/singular/deploy"
-	"github.com/Huawei/containerops/singular/download"
-	"github.com/Huawei/containerops/singular/vm"
 )
 
 func main() {
 
 	cmd.Execute()
-	// create vmlist
-	vm.CreateVMs()
-	//vm.CreateNewVM("lidian-unbantu-wk-master")
-	//get iplist while
-	download.Download_main()
-	deploy.DeployNodes()
 
 }
