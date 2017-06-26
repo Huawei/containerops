38e4c49d3a7a5b78573cbb64b7e9d7e07d787019
Meaglith Ma <maquanyi@huawei.com>
2017-06-26 17:30:43 +0800

    Add Flow and other structs.

--- pilotage/module/module.go
+++ pilotage/module/module.go
@@ -16,6 +16,54 @@ limitations under the License.
 
 package module
 
-type Flow struct {
+const (
+	RunModelCli    = "CLI"
+	RunModelDaemon = "DAEMON"
+)
 
+// Flow is DevOps orchestration flow struct.
+type Flow struct {
+	// Not read file or database and it's a runtime value.
+	Model string `json:"model" yaml:"model"`
+	// The top level of flow
+	Name    string `json:"name" yaml:"name"`
+	Title   string `json:"title" yaml:"title"`
+	Version int64  `json:"version" yaml:"version"`
+	Tag     string `json:"tag" yaml:"tag"`
+	Timeout int64  `json:"timeout" yaml:"timeout"`
+	// The Stages
+	Stages []Stage `json:"stages" yaml:"stages"`
+}
+
+// Stage is
+type Stage struct {
+	T          string   `json:"type" yaml:"type"`
+	Name       string   `json:"name" yaml:"name"`
+	Title      string   `json:"title" yaml:"title"`
+	Sequencing string   `json:"sequencing" yaml:"sequencing"`
+	Actions    []Action `json:"action" yaml:"action"`
+}
+
+// Action is
+type Action struct {
+	Name  string `json:"name" yaml:"name"`
+	Title string `json:"title" yaml:"title"`
+	Jobs  []Job  `json:"jobs" yaml:"jobs"`
+}
+
+// Job is
+type Job struct {
+	T            string              `json:"type" yaml:"type"`
+	Kubectl      string              `json:"kubectl" yaml:"kubectl"`
+	Endpoint     string              `json:"endpoint" yaml:"endpoint"`
+	Timeout      string              `json:"timeout" yaml:"timeout"`
+	Resources    Resource            `json:"resources" yaml:"resources"`
+	Environments []map[string]string `json:"environments" yaml:"environments"`
+	Output       []string            `json:"output" yaml:"output"`
+}
+
+// Resources is
+type Resource struct {
+	CPU    string `json:"cpu" yaml:"cpu"`
+	Memory string `json:"memory" yaml:"memory"`
 }
