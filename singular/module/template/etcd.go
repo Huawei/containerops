c0e29fdff7a28b3f6215648b78a3baf3691eef6f
Quanyi Ma <maquanyi@huawei.com>
2017-07-22 20:16:23 +0800

    SSH to vm and execute update command.
    Signed-off-by: Quanyi Ma <maquanyi@huawei.com>

--- singular/module/template/etcd.go
+++ singular/module/template/etcd.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 package template
 
-var Etcd = map[string]string{
+var EtcdCATemplate = map[string]string{
 	"etcd-3.2.2": `
 {
 	"CN": "etcd",
@@ -38,4 +38,40 @@ var Etcd = map[string]string{
 		}
 	]
 }`,
+}
+
+var EtcdSystemdTemplate = map[string]string{
+	"etcd-3.2.2": `
+[Unit]
+Description=Etcd Server
+After=network.target
+After=network-online.target
+Wants=network-online.target
+Documentation=https://github.com/coreos
+
+[Service]
+Type=notify
+WorkingDirectory=/var/lib/etcd/
+ExecStart=/usr/local/bin/etcd \\
+  --name={{.Name}} \\
+  --cert-file=/etc/etcd/ssl/etcd.pem \\
+  --key-file=/etc/etcd/ssl/etcd-key.pem \\
+  --peer-cert-file=/etc/etcd/ssl/etcd.pem \\
+  --peer-key-file=/etc/etcd/ssl/etcd-key.pem \\
+  --trusted-ca-file=/etc/kubernetes/ssl/ca.pem \\
+  --peer-trusted-ca-file=/etc/kubernetes/ssl/ca.pem \\
+  --initial-advertise-peer-urls=https://{{.IP}}:2380 \\
+  --listen-peer-urls=https://{{.IP}}:2380 \\
+  --listen-client-urls=https://{{.IP}}:2379,http://127.0.0.1:2379 \\
+  --advertise-client-urls=https://{{.IP}}:2379 \\
+  --initial-cluster-token=etcd-cluster-0 \\
+  --initial-cluster={{.Nodes}} \\
+  --initial-cluster-state=new \\
+  --data-dir=/var/lib/etcd
+Restart=on-failure
+RestartSec=5
+LimitNOFILE=65536
+
+[Install]
+WantedBy=multi-user.target`,
 }
