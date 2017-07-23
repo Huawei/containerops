6c65a95a65179aa180802e026216f06b21bc3322
crystaldust <juzhen@huawei.com>
2017-07-23 19:12:02 +0000

    Add the singular demo for cncf

---
+++ demo/cncf/singular/README.md
@@ -0,0 +1,63 @@
+## Deploy a kubernetes cluster with ContainerOps singular
+
+```bash
+docker build -t hub.opshub.sh/containerops/cncf-demo-singular .
+```
+
+
+```bash
+docker run --env CO_DATA="\
+	token=435a054fb1f81e439a63a608eddb67208a66cb11d6b7abeaa3d89aac777d4d1d \
+	kube_apiserver_url=https://hub.opshub.sh/binary/v1/containerops/singular/binary/kube-apiserver/1.6.7 \
+	kube_controllermanager_url=https://hub.opshub.sh/binary/v1/containerops/singular/binary/kube-controller-manager/1.6.7 \
+	kube_scheduler_url=https://hub.opshub.sh/binary/v1/containerops/singular/binary/kube-scheduler/1.6.7 \
+	kubectl_url=https://hub.opshub.sh/binary/v1/containerops/singular/binary/kubectl/1.6.7 \
+	kubelete_url=https://hub.opshub.sh/binary/v1/containerops/singular/binary/kubelet/1.6.7 \
+	kube_proxy_url=https://hub.opshub.sh/binary/v1/containerops/singular/binary/kube-proxy/1.6.7" \
+hub.opshub.sh/containerops/cncf-demo-singular:latest
+
+```
+
+```dockerfile
+FROM hub.opshub.sh/containerops/golang:1.8.3
+MAINTAINER Zhen Ju <juzhenatpku@gmail.com>
+
+USER root
+RUN apt-get update && apt-get install -y gcc make g++
+ENV PATH $PATH:$GOPATH/src/github.com/Huawei/containerops
+
+RUN mkdir -p $GOPATH/src/github.com/Huawei/
+
+WORKDIR $GOPATH/src/github.com/Huawei/
+RUN git clone https://github.com/Huawei/containerops
+
+# The essential dependencies for singular
+RUN go get "github.com/cloudflare/cfssl/cli"
+RUN go get "github.com/cloudflare/cfssl/cli/genkey"
+RUN go get "github.com/cloudflare/cfssl/cli/sign"
+RUN go get "github.com/cloudflare/cfssl/csr"
+RUN go get "github.com/cloudflare/cfssl/initca"
+RUN go get "github.com/cloudflare/cfssl/signer"
+RUN go get "github.com/digitalocean/godo"
+RUN go get "github.com/fernet/fernet-go"
+RUN go get "github.com/logrusorgru/aurora"
+RUN go get "github.com/mitchellh/go-homedir"
+RUN go get "github.com/pkg/sftp"
+RUN go get "github.com/spf13/cobra"
+RUN go get "github.com/spf13/viper"
+RUN go get "golang.org/x/crypto/ssh"
+RUN go get "golang.org/x/net/context"
+RUN go get "golang.org/x/oauth2"
+RUN go get "gopkg.in/yaml.v2"
+
+WORKDIR $GOPATH/src/github.com/Huawei/containerops/singular/
+RUN go build
+
+WORKDIR $GOPATH/src/github.com/Huawei/containerops/demo/cncf/singular/codes/
+RUN go build -o ../singular-demo
+
+WORKDIR $GOPATH/src/github.com/Huawei/containerops/demo/cncf/singular/
+RUN cp $GOPATH/src/github.com/Huawei/containerops/singular/singular ./
+
+CMD ./singular-demo
+```
---
+++ demo/cncf/singular/codes/singular.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	data := os.Getenv("CO_DATA")
+	if len(data) == 0 {
+		fmt.Fprintf(os.Stderr, "[COUT] %s\n", "The CO_DATA value is null.")
+		fmt.Fprintf(os.Stdout, "[COUT] CO_RESULT = %s\n", "The CO_DATA value is null.")
+		os.Exit(1)
+	}
+
+	token, kubeApiServerUrl, kubeControllerManagerUrl, kubeSchedulerUrl, kubectlUrl, kubeleteUrl, kubeProxyUrl, err := parseEnv(data)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[COUT] Parse the CO_DATA error: %s\n", err.Error())
+		fmt.Fprintf(os.Stdout, "[COUT] CO_RESULT = %s\n", "false")
+		os.Exit(1)
+	}
+
+	bs, err := ioutil.ReadFile("./singular.template.yaml")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "COUT] Failed to read the template file: %s", err.Error())
+		os.Exit(1)
+	}
+	singularTemplate := string(bs)
+
+	singular := map[string]interface{}{
+		"Token":                    token,
+		"KubeApiServerUrl":         kubeApiServerUrl,
+		"KubeControllerManagerUrl": kubeControllerManagerUrl,
+		"KubeSchedulerUrl":         kubeSchedulerUrl,
+		"KubectlUrl":               kubectlUrl,
+		"KubeletUrl":               kubeleteUrl,
+		"KubeProxyUrl":             kubeProxyUrl,
+	}
+	t := template.Must(template.New("Singular").Parse(singularTemplate))
+	var buf bytes.Buffer
+	err = t.Execute(&buf, singular)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to generate yaml from template: %s", err.Error())
+		os.Exit(1)
+	}
+
+	// Write the yaml to file
+	if err := ioutil.WriteFile("./singular.yaml", buf.Bytes(), os.ModePerm); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to write yaml into file: %s", err.Error())
+		os.Exit(1)
+	}
+
+	// Call singular binary to deploy the k8s cluster
+	// Since the Provider and Token is provied in the yaml, the config file is not necessary here, create a mock
+	mockConfigContent := `
+	[singular]
+	provider="digitalocean"
+	token="helloworld"
+	`
+	if err := ioutil.WriteFile("./runtime.toml", []byte(mockConfigContent), os.ModePerm); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to generate mock config: %s", err.Error())
+		os.Exit(1)
+	}
+	cmd := exec.Command("./singular", "deploy", "template", "./singular.yaml", "--config", "./runtime.toml", "--verbose", "true")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to deploy k8s cluster with singular: %s", err.Error())
+		os.Exit(1)
+	}
+	fmt.Fprintf(os.Stdout, "[COUT] CO_RESULT=true")
+}
+
+// Parse CO_DATA value, and return token, and the urls of kubernetes components
+func parseEnv(env string) (token, kubeApiServerUrl, kubeControllerManagerUrl, kubeSchedulerUrl, kubectlUrl, kubeleteUrl, kubeProxyUrl string, err error) {
+	files := strings.Fields(env)
+	if len(files) == 0 {
+		err = fmt.Errorf("CO_DATA value is null\n")
+		return
+	}
+
+	for _, v := range files {
+		s := strings.Split(v, "=")
+		key, value := s[0], s[1]
+
+		switch key {
+		case "token":
+			token = value
+		case "kube_apiserver_url":
+			kubeApiServerUrl = value
+		case "kube_controllermanager_url":
+			kubeControllerManagerUrl = value
+		case "kube_scheduler_url":
+			kubeSchedulerUrl = value
+		case "kubectl_url":
+			kubectlUrl = value
+		case "kubelete_url":
+			kubeleteUrl = value
+		case "kube_proxy_url":
+			kubeProxyUrl = value
+		default:
+			fmt.Fprintf(os.Stdout, "[COUT] Unknown Parameter: [%s]\n", s)
+		}
+	}
+	return
+}
