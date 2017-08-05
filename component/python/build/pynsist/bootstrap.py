fc4593879219343653e3cc49b761b9363a5efcfb
Lupino <lmjubuntu@gmail.com>
2017-08-05 21:40:04 +0800

    pynsist: build windows installers for python applications
    pynsist is a tool to build Windows installers for your Python
    applications. The installers bundle Python itself,
    so you can distribute your application to people
    who don't have Python installed.

---
+++ component/python/build/pynsist/bootstrap.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+
+import subprocess
+import os
+import sys
+import glob
+
+REPO_PATH = 'git-repo'
+
+
+def git_clone(url):
+    r = subprocess.run(['git', 'clone', url, REPO_PATH])
+
+    if r.returncode == 0:
+        return True
+    else:
+        print("[COUT] Git clone error: Invalid argument to exit",
+              file=sys.stderr)
+        print("[COUT] CO_RESULT = false")
+        return False
+
+
+def setup(path):
+    file_name = os.path.basename(path)
+    dir_name = os.path.dirname(path)
+    r = subprocess.run('cd {}; python3 {} install'.format(dir_name, file_name),
+                       shell=True)
+
+    if r.returncode != 0:
+        print("[COUT] install dependences failed: {}".format(path), file=sys.stderr)
+        return False
+
+    return True
+
+
+def pip_install(file_name):
+    r = subprocess.run(['pip3', 'install', '-r', file_name])
+
+    if r.returncode != 0:
+        print("[COUT] install dependences failed: {}".format(path), file=sys.stderr)
+        return False
+
+    return True
+
+
+def upload_file(upload):
+    parsed = upload.split('/')
+    host = parsed[0]
+    namespace = parsed[1]
+    repo = parsed[2]
+    binary = parsed[3]
+    tag = parsed[4]
+    url = 'https://{}/binary/v1/{}/{}/binary/{}/{}'.format(host, namespace,
+                                                           repo, binary, tag)
+
+    r1 = subprocess.run(['curl', '-XPUT', '-d', '@/tmp/output.tar.bz2', url])
+    if r1.returncode != 0:
+        print("[COUT] upload error", file=sys.stderr)
+        return False
+    return True
+
+
+def pynsist(file_name):
+    r = subprocess.run(['pynsist', '{}/{}'.format(REPO_PATH, file_name)])
+
+    if r.returncode != 0:
+        print("[COUT] pynsist error", file=sys.stderr)
+        return False
+
+    return True
+
+
+def compress(file_name):
+    dirname = os.path.dirname(file_name)
+    r = subprocess.run('cd {}/{}/build/nsis; tar cjvf /tmp/output.tar.bz2 .'.format(REPO_PATH, dirname), shell=True)
+
+    if r.returncode != 0:
+        print("[COUT] compress error", file=sys.stderr)
+        return False
+
+    return True
+
+
+def parse_argument():
+    data = os.environ.get('CO_DATA', None)
+    if not data:
+        return {}
+
+    validate = ['git-url', 'entry-file', 'upload']
+    ret = {}
+    for s in data.split(' '):
+        s = s.strip()
+        if not s:
+            continue
+        arg = s.split('=')
+        if len(arg) < 2:
+            print('[COUT] Unknown Parameter: [{}]'.format(s))
+            continue
+
+        if arg[0] not in validate:
+            print('[COUT] Unknown Parameter: [{}]'.format(s))
+            continue
+
+        ret[arg[0]] = arg[1]
+
+    return ret
+
+
+def main():
+    argv = parse_argument()
+    git_url = argv.get('git-url')
+    if not git_url:
+        print("[COUT] The git-url value is null", file=sys.stderr)
+        print("[COUT] CO_RESULT = false")
+        return
+
+    entry_file = argv.get('entry-file')
+    if not entry_file:
+        print("[COUT] The entry-file value is null", file=sys.stderr)
+        print("[COUT] CO_RESULT = false")
+        return
+
+    upload = argv.get('upload')
+    if not upload:
+        print("[COUT] The upload value is null", file=sys.stderr)
+        print("[COUT] CO_RESULT = false")
+        return
+
+    if not git_clone(git_url):
+        return
+
+    if not pynsist(entry_file):
+        print("[COUT] CO_RESULT = false")
+        return
+
+    if not compress(entry_file):
+        print("[COUT] CO_RESULT = false")
+        return
+
+    if not upload_file(upload):
+        print("[COUT] CO_RESULT = false")
+        return
+
+    print("[COUT] CO_RESULT = true")
+
+
+main()
