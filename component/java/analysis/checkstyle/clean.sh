5d8145635387883faf1f00535a2f10674c8e80cb
vanniuner <at18708169062@gmail.com>
2017-08-06 10:51:54 +0800

    add gradle pmd component
    pmd is a source code analyzer. It finds common programming flaws like unused variables, empty catch blocks, unnecessary object creation, and so forth
    
    Ref: https://pmd.github.io/

--- component/java/analysis/checkstyle/clean.sh
+++ component/java/analysis/checkstyle/clean.sh
@@ -2,4 +2,4 @@
 docker stop $(docker ps | awk '{ print $1 }')
 docker rm $(docker ps -a | awk '{ print $1 }')
 docker rmi $(docker images | awk '/^<none>/ { print $3 }')
-docker rmi $(docker images | awk '/^`cat imagename` { print $3 }')
+docker rmi $(docker images | awk '/^huawei\/analysis\/checkstyle/ { print $3 }')
