3e9f8793185124239e3d08638c703a096614138e
van <at18708169062@gmail.com>
2017-08-06 10:45:22 +0800

    delete clean shell script
    delete clean shell script

--- component/java/analysis/checkstyle/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker stop $(docker ps | awk '{ print $1 }')
-docker rm $(docker ps -a | awk '{ print $1 }')
-docker rmi $(docker images | awk '/^<none>/ { print $3 }')
-docker rmi $(docker images | awk '/^huawei\/analysis\/checkstyle/ { print $3 }')
