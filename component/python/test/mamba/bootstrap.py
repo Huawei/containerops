230e70ef8d444374137a36c473c0026a23d3b2d2
Lupino <lmjubuntu@gmail.com>
2017-08-12 14:18:00 +0800

    mamba: the definitive testing tool for python
    mamba is the definitive test runner for Python. Born under the banner of
    behavior-driven development.
    
    In this component we only parse the coverage result on yaml format

---
+++ component/python/test/mamba/bootstrap.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+
+import subprocess
+import os
+import sys
+import glob
+import json
+import yaml
+
+REPO_PATH = 'git-repo'
+
+
+def git_clone(url):
+    r = subprocess.run(['git', 'clone', url, REPO_PATH])
+
+    if r.returncode == 0:
+        return True
+    else:
+        print("[COUT] Git clone error: Invalid argument to exit",
+              file=sys.stderr)
+        print("[COUT] CO_RESULT = false")
+        return False
+
+
+def setup(path):
+    file_name = os.path.basename(path)
+    dir_name = os.path.dirname(path)
+    r = subprocess.run('cd {}; python3 {} install'.format(dir_name, file_name),
+                       shell=True)
+
+    if r.returncode != 0:
+        print("[COUT] install dependences failed: {}".format(path), file=sys.stderr)
+        return False
+
+    return True
+
+
+def pip_install(file_name):
+    r = subprocess.run(['pip3', 'install', '-r', file_name])
+
+    if r.returncode != 0:
+        print("[COUT] install dependences failed: {}".format(file_name), file=sys.stderr)
+        return False
+
+    return True
+
+
+def mamba(file_name):
+    r = subprocess.run('cd {}; mamba {} --enable-coverage'.format(REPO_PATH, file_name), shell=True)
+
+    if r.returncode != 0:
+        print("[COUT] mamba error", file=sys.stderr)
+        return False
+
+    return True
+
+
+def echo_yaml():
+    file_name = '{}/.coverage'.format(REPO_PATH)
+    data = open(file_name).read()
+    idx = data.find('{')
+    data = data[idx:]
+    data = json.loads(data)
+    lines = yaml.safe_dump(data)
+    for line in lines.split('\n'):
+        print('[COUT] CO_YAML_CONTENT {}'.format(line))
+
+
+def parse_argument():
+    data = os.environ.get('CO_DATA', None)
+    if not data:
+        return {}
+
+    validate = ['git-url', 'entry-file']
+    ret = {}
+    for s in data.split(' '):
+        s = s.strip()
+        if not s:
+            continue
+        arg = s.split('=')
+        if len(arg) < 2:
+            print('[COUT] Unknown Parameter: [{}]'.format(s))
+            continue
+
+        if arg[0] not in validate:
+            print('[COUT] Unknown Parameter: [{}]'.format(s))
+            continue
+
+        ret[arg[0]] = arg[1]
+
+    return ret
+
+
+def main():
+    argv = parse_argument()
+    git_url = argv.get('git-url')
+    if not git_url:
+        print("[COUT] The git-url value is null", file=sys.stderr)
+        print("[COUT] CO_RESULT = false")
+        return
+
+    entry_file = argv.get('entry-file')
+    if not entry_file:
+        print("[COUT] The entry-file value is null", file=sys.stderr)
+        print("[COUT] CO_RESULT = false")
+        return
+
+    if not git_clone(git_url):
+        return
+
+    for file_name in glob.glob('{}/setup.py'.format(REPO_PATH)):
+        setup(file_name)
+
+    for file_name in glob.glob('{}/*/setup.py'.format(REPO_PATH)):
+        setup(file_name)
+
+    for file_name in glob.glob('{}/requirements.txt'.format(REPO_PATH)):
+        pip_install(file_name)
+
+    for file_name in glob.glob('{}/*/requirements.txt'.format(REPO_PATH)):
+        pip_install(file_name)
+
+    for file_name in glob.glob('{}/requirements-dev.txt'.format(REPO_PATH)):
+        pip_install(file_name)
+
+    for file_name in glob.glob('{}/*/requirements-dev.txt'.format(REPO_PATH)):
+        pip_install(file_name)
+
+
+    out = mamba(entry_file)
+    echo_yaml()
+
+    if not out:
+        print("[COUT] CO_RESULT = false")
+        return
+
+    print("[COUT] CO_RESULT = true")
+
+
+main()
