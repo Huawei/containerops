e3d0527190e3ad069714007279576a52bd01cb50
Lupino <lmjubuntu@gmail.com>
2017-08-14 20:18:22 +0800

    memory_profiler: python2 support with a spec param version

--- component/python/analysis/memory_profiler/README.md
+++ component/python/analysis/memory_profiler/README.md
@@ -9,3 +9,12 @@ docker build -t containerops/memory_profiler .
 ```bash
 docker run --rm -e CO_DATA='git-url=https://github.com/fabianp/memory_profiler.git entry-file=test/test_func.py' containerops/memory_profiler
 ```
+
+## TEST with deference python version
+
+`version` is one of `python`, `python2`, `python3`, `py3k`.
+default is `py3k`
+
+```bash
+docker run --rm -e CO_DATA='git-url=https://github.com/fabianp/memory_profiler.git entry-file=test/test_func.py version=python' containerops/memory_profiler
+```
--- component/python/analysis/memory_profiler/bootstrap.py
+++ component/python/analysis/memory_profiler/bootstrap.py
@@ -4,7 +4,6 @@ import subprocess
 import os
 import sys
 import glob
-import linecache
 
 REPO_PATH = 'git-repo'
 
@@ -20,31 +19,58 @@ def git_clone(url):
         return False
 
 
-def setup(path):
+def get_pip_cmd(version):
+    if version == 'py3k' or version == 'python3':
+        return 'pip3'
+
+    return 'pip'
+
+
+def get_python_cmd(version):
+    if version == 'py3k' or version == 'python3':
+        return 'python3'
+
+    return 'python'
+
+
+def init_env(version):
+    subprocess.run([get_pip_cmd(version), 'install', 'memory_profiler', 'pyyaml', 'psutil'])
+
+
+def validate_version(version):
+    valid_version = ['python', 'python2', 'python3', 'py3k']
+    if version not in valid_version:
+        print("[COUT] Check version failed: the valid version is {}".format(valid_version), file=sys.stderr)
+        return False
+
+    return True
+
+
+def setup(path, version='py3k'):
     file_name = os.path.basename(path)
     dir_name = os.path.dirname(path)
-    r = subprocess.run('cd {}; python3 {} install'.format(dir_name, file_name),
+    r = subprocess.run('cd {}; {} {} install'.format(dir_name, get_python_cmd(version), file_name),
                        shell=True)
 
     if r.returncode != 0:
-        print("[COUT] install dependences failed: {}".format(path), file=sys.stderr)
+        print("[COUT] install dependences failed", file=sys.stderr)
         return False
 
     return True
 
 
-def pip_install(file_name):
-    r = subprocess.run(['pip3', 'install', '-r', file_name])
+def pip_install(file_name, version='py3k'):
+    r = subprocess.run([get_pip_cmd(version), 'install', '-r', file_name])
 
     if r.returncode != 0:
-        print("[COUT] install dependences failed: {}".format(path), file=sys.stderr)
+        print("[COUT] install dependences failed", file=sys.stderr)
         return False
 
     return True
 
 
-def memory_profiler(file_name):
-    r = subprocess.run(['python3', '/root/memory_profiler.py',
+def memory_profiler(file_name, version='py3k'):
+    r = subprocess.run([get_python_cmd(version), '/root/memory_profiler.py',
                         os.path.join(REPO_PATH, file_name)])
 
     passed = True
@@ -59,7 +85,7 @@ def parse_argument():
     if not data:
         return {}
 
-    validate = ['git-url', 'entry-file']
+    validate = ['git-url', 'entry-file', 'version']
     ret = {}
     for s in data.split(' '):
         s = s.strip()
@@ -87,8 +113,16 @@ def main():
         print("[COUT] CO_RESULT = false")
         return
 
+    version = argv.get('version', 'py3k')
+
+    if not validate_version(version):
+        print("[COUT] CO_RESULT = false")
+        return
+
+    init_env(version)
+
     entry_file = argv.get('entry-file')
-    if not git_url:
+    if not entry_file:
         print("[COUT] The entry-file value is null", file=sys.stderr)
         print("[COUT] CO_RESULT = false")
         return
@@ -96,19 +130,19 @@ def main():
     if not git_clone(git_url):
         return
 
-    for file_name in glob.glob('{}/setup.py'.format(REPO_PATH)):
-        setup(file_name)
+    for file_name in glob.glob('./*/setup.py'):
+        setup(file_name, version)
 
-    for file_name in glob.glob('{}/*/setup.py'.format(REPO_PATH)):
-        setup(file_name)
+    for file_name in glob.glob('./*/*/setup.py'):
+        setup(file_name, version)
 
-    for file_name in glob.glob('{}/requirements.txt'.format(REPO_PATH)):
-        pip_install(file_name)
+    for file_name in glob.glob('./*/requirements.txt'):
+        pip_install(file_name, version)
 
-    for file_name in glob.glob('{}/*/requirements.txt'.format(REPO_PATH)):
-        pip_install(file_name)
+    for file_name in glob.glob('./*/*/requirements.txt'):
+        pip_install(file_name, version)
 
-    out = memory_profiler(entry_file)
+    out = memory_profiler(entry_file, version)
 
     if out:
         print("[COUT] CO_RESULT = true")
