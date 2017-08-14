c501ea4ce2a40cc333a03b1623468ba36e70973b
Lupino <lmjubuntu@gmail.com>
2017-08-14 21:53:30 +0800

    unittest: python2 support with a spec param version

--- component/python/test/unittest/README.md
+++ component/python/test/unittest/README.md
@@ -9,3 +9,12 @@ docker build -t containerops/unittest .
 ```bash
 docker run --rm -e CO_DATA='git-url=https://github.com/minhhh/regex.git entry-path=.' containerops/unittest
 ```
+
+## TEST with deference python version
+
+`version` is one of `python`, `python2`, `python3`, `py3k`.
+default is `py3k`
+
+```bash
+docker run --rm -e CO_DATA='git-url=https://github.com/minhhh/regex.git entry-path=. version=python' containerops/unittest
+```
--- component/python/test/unittest/bootstrap.py
+++ component/python/test/unittest/bootstrap.py
@@ -20,31 +20,58 @@ def git_clone(url):
         return False
 
 
-def setup(path):
+def get_pip_cmd(version):
+    if version == 'py3k' or version == 'python3':
+        return 'pip3'
+
+    return 'pip'
+
+
+def get_python_cmd(version):
+    if version == 'py3k' or version == 'python3':
+        return 'python3'
+
+    return 'python'
+
+
+def init_env(version):
+    subprocess.run([get_pip_cmd(version), 'install', 'unittest-xml-reporting'])
+
+
+def validate_version(version):
+    valid_version = ['python', 'python2', 'python3', 'py3k']
+    if version not in valid_version:
+        print("[COUT] Check version failed: the valid version is {}".format(valid_version), file=sys.stderr)
+        return False
+
+    return True
+
+
+def setup(path, version='py3k'):
     file_name = os.path.basename(path)
     dir_name = os.path.dirname(path)
-    r = subprocess.run('cd {}; python3 {} install'.format(dir_name, file_name),
+    r = subprocess.run('cd {}; {} {} install'.format(dir_name, get_python_cmd(version), file_name),
                        shell=True)
 
     if r.returncode != 0:
-        print("[COUT] install dependences failed: {}".format(path), file=sys.stderr)
+        print("[COUT] install dependences failed", file=sys.stderr)
         return False
 
     return True
 
 
-def pip_install(file_name):
-    r = subprocess.run(['pip3', 'install', '-r', file_name])
+def pip_install(file_name, version='py3k'):
+    r = subprocess.run([get_pip_cmd(version), 'install', '-r', file_name])
 
     if r.returncode != 0:
-        print("[COUT] install dependences failed: {}".format(file_name), file=sys.stderr)
+        print("[COUT] install dependences failed", file=sys.stderr)
         return False
 
     return True
 
 
-def pytest(file_name):
-    r = subprocess.run('cd {}/{}; python3 /root/xmlrunner'.format(REPO_PATH, file_name), shell=True)
+def unittest(file_name, version='py3k'):
+    r = subprocess.run('cd {}/{};{} /root/xmlrunner'.format(REPO_PATH, file_name, get_python_cmd(version)), shell=True)
 
     if r.returncode != 0:
         return False
@@ -69,7 +96,7 @@ def parse_argument():
     if not data:
         return {}
 
-    validate = ['git-url', 'entry-path']
+    validate = ['git-url', 'entry-path', 'version']
     ret = {}
     for s in data.split(' '):
         s = s.strip()
@@ -97,6 +124,14 @@ def main():
         print("[COUT] CO_RESULT = false")
         return
 
+    version = argv.get('version', 'py3k')
+
+    if not validate_version(version):
+        print("[COUT] CO_RESULT = false")
+        return
+
+    init_env(version)
+
     entry_path = argv.get('entry-path')
     if not entry_path:
         print("[COUT] The entry-path value is null", file=sys.stderr)
@@ -107,25 +142,25 @@ def main():
         return
 
     for file_name in glob.glob('{}/setup.py'.format(REPO_PATH)):
-        setup(file_name)
+        setup(file_name, version)
 
     for file_name in glob.glob('{}/*/setup.py'.format(REPO_PATH)):
-        setup(file_name)
+        setup(file_name, version)
 
     for file_name in glob.glob('{}/requirements.txt'.format(REPO_PATH)):
-        pip_install(file_name)
+        pip_install(file_name, version)
 
     for file_name in glob.glob('{}/*/requirements.txt'.format(REPO_PATH)):
-        pip_install(file_name)
+        pip_install(file_name, version)
 
     for file_name in glob.glob('{}/requirements_dev.txt'.format(REPO_PATH)):
-        pip_install(file_name)
+        pip_install(file_name, version)
 
     for file_name in glob.glob('{}/*/requirements_dev.txt'.format(REPO_PATH)):
-        pip_install(file_name)
+        pip_install(file_name, version)
 
 
-    out = pytest(entry_path)
+    out = unittest(entry_path, version)
     echo_xml()
 
     if not out:
