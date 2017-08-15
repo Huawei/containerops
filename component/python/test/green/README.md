511ed011f37d6c5debd46bd1f310491d6fc52a15
Lupino <lmjubuntu@gmail.com>
2017-08-15 19:54:51 +0800

    green: python2 support with a spec param version

--- component/python/test/green/README.md
+++ component/python/test/green/README.md
@@ -9,3 +9,12 @@ docker build -t containerops/green .
 ```bash
 docker run --rm -e CO_DATA='git-url=https://github.com/minhhh/regex.git entry-path=.' containerops/green
 ```
+
+## TEST with deference python version
+
+`version` is one of `python`, `python2`, `python3`, `py3k`.
+default is `py3k`
+
+```bash
+docker run --rm -e CO_DATA='git-url=https://github.com/minhhh/regex.git entry-path=. version=python' containerops/green
+```
--- component/python/test/green/bootstrap.py
+++ component/python/test/green/bootstrap.py
@@ -22,24 +22,51 @@ def git_clone(url):
         return False
 
 
-def setup(path):
-    file_name = os.path.basename(path)
-    dir_name = os.path.dirname(path)
-    r = subprocess.run('cd {}; python3 {} install'.format(dir_name, file_name),
-                       shell=True)
+def get_pip_cmd(version):
+    if version == 'py3k' or version == 'python3':
+        return 'pip3'
 
-    if r.returncode != 0:
-        print("[COUT] install dependences failed: {}".format(path), file=sys.stderr)
+    return 'pip'
+
+
+def get_python_cmd(version):
+    if version == 'py3k' or version == 'python3':
+        return 'python3'
+
+    return 'python'
+
+
+def init_env(version):
+    subprocess.run([get_pip_cmd(version), 'install', 'green', 'coverage'])
+
+
+def validate_version(version):
+    valid_version = ['python', 'python2', 'python3', 'py3k']
+    if version not in valid_version:
+        print("[COUT] Check version failed: the valid version is {}".format(valid_version), file=sys.stderr)
         return False
 
     return True
 
 
-def pip_install(file_name):
-    r = subprocess.run(['pip3', 'install', '-r', file_name])
+def setup(path, version='py3k'):
+    file_name = os.path.basename(path)
+    dir_name = os.path.dirname(path)
+    r = subprocess.run('cd {}; {} {} install'.format(dir_name, get_python_cmd(version), file_name),
+                       shell=True)
 
     if r.returncode != 0:
-        print("[COUT] install dependences failed: {}".format(file_name), file=sys.stderr)
+        print("[COUT] install dependences failed", file=sys.stderr)
+        return False
+
+    return True
+
+
+def pip_install(file_name, version='py3k'):
+    r = subprocess.run([get_pip_cmd(version), 'install', '-r', file_name])
+
+    if r.returncode != 0:
+        print("[COUT] install dependences failed", file=sys.stderr)
         return False
 
     return True
@@ -79,7 +106,7 @@ def parse_argument():
     if not data:
         return {}
 
-    validate = ['git-url', 'entry-path']
+    validate = ['git-url', 'entry-path', 'version']
     ret = {}
     for s in data.split(' '):
         s = s.strip()
@@ -107,6 +134,14 @@ def main():
         print("[COUT] CO_RESULT = false")
         return
 
+    version = argv.get('version', 'py3k')
+
+    if not validate_version(version):
+        print("[COUT] CO_RESULT = false")
+        return
+
+    init_env(version)
+
     entry_path = argv.get('entry-path')
     if not entry_path:
         print("[COUT] The entry-path value is null", file=sys.stderr)
@@ -117,23 +152,22 @@ def main():
         return
 
     for file_name in glob.glob('{}/setup.py'.format(REPO_PATH)):
-        setup(file_name)
+        setup(file_name, version)
 
     for file_name in glob.glob('{}/*/setup.py'.format(REPO_PATH)):
-        setup(file_name)
+        setup(file_name, version)
 
     for file_name in glob.glob('{}/requirements.txt'.format(REPO_PATH)):
-        pip_install(file_name)
+        pip_install(file_name, version)
 
     for file_name in glob.glob('{}/*/requirements.txt'.format(REPO_PATH)):
-        pip_install(file_name)
+        pip_install(file_name, version)
 
     for file_name in glob.glob('{}/requirements_dev.txt'.format(REPO_PATH)):
-        pip_install(file_name)
+        pip_install(file_name, version)
 
     for file_name in glob.glob('{}/*/requirements_dev.txt'.format(REPO_PATH)):
-        pip_install(file_name)
-
+        pip_install(file_name, version)
 
     out = green(entry_path)
 
