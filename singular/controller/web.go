62281e3069c775416f3499e9d1ee1976295db24e
Quanyi Ma <maquanyi@huawei.com>
2017-09-13 23:26:27 +0800

    Fix bug of component Name to Binary
    Signed-off-by: Quanyi Ma <maquanyi@huawei.com>

--- singular/controller/web.go
+++ singular/controller/web.go
@@ -150,7 +150,7 @@ func convertToBr(src string) template.HTML {
 
 func convertComponent(input *model.ComponentV1) HtmlComponent {
 	c := HtmlComponent{
-		Name: input.Name,
+		Name: input.Binary,
 		Log:  convertToBr(input.Log),
 	}
 	// Got the size and icon
