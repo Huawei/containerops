d5bc5098448f2fdbd7dffa3998aa42c2d2bb2b83
DeanLee <lidian1@huawei.com>
2017-10-18 20:46:17 +0800

    add  main.go file for output log

--- component/ctest/ci-component-run.sh
+++ component/ctest/ci-component-run.sh
@@ -1,1 +1,2 @@
- -X POST -H 'Content-type':'application/yaml' --data-binary @ci-component.yml  https://flow.opshub.sh/flow/v1/containerops/component-ctest-flow/flow/latest/yaml
+ #!/bin/bash
+curl -i -X POST -H 'Content-type':'application/yaml' --data-binary @ci-component.yml  https://flow.opshub.sh/flow/v1/containerops/component-ctest-flow/flow/latest/yaml
---
+++ component/ctest/flow/main.go
@@ -0,0 +1,15 @@
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+)
+
+
+func main() {
+
+	fmt.Fprintf(os.Stdout, "[COUT] CO_RESULT = %s\n", "false")
+	
+}
