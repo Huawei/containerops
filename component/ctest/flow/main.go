292360167ce13fd22e6831190f093eb843de16f9
DeanLee <lidian1@huawei.com>
2017-10-19 15:17:00 +0800

    update main.go logic about print error and result

--- component/ctest/flow/main.go
+++ component/ctest/flow/main.go
@@ -1,15 +1,20 @@
-
 package main
 
 import (
 	"fmt"
 	"os"
-
 )
-
 
 func main() {
 
+	//Get the CO_DATA from environment parameter "CO_DATA"
+	data := os.Getenv("CO_DATA")
+	if len(data) == 0 {
+		fmt.Fprintf(os.Stderr, "[COUT] %s\n", "The CO_DATA value is null.")
+		fmt.Fprintf(os.Stdout, "[COUT] CO_RESULT = %s\n", "false")
+		os.Exit(1)
+	}
+
 	fmt.Fprintf(os.Stdout, "[COUT] CO_RESULT = %s\n", "false")
-	
+
 }
