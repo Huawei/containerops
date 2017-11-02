d7b736da91c0bbfe7de79f24dd2f7dfc1c048a2c
liuzijie <fred.liu@ximalaya.com>
2017-11-02 17:00:41 +0800

    fix paramter

--- component/java/compile/ear/run.sh
+++ component/java/compile/ear/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 IMAGE_NAME=`cat imagename`
-docker run --rm --env CO_DATA="git-url=https://github.com/wangkirin/demo-bmi.git target=https://hub.opshub.sh/binary/v1/containerops/component/binary/2.2.4/demo.ear version=gradle3 build-path=./webapps" $IMAGE_NAME
+docker run --rm --env CO_DATA="git-url=https://github.com/wangkirin/demo-bmi.git target=https://hub.opshub.sh/binary/v1/containerops/component/binary/2.2.4/demo.ear version=gradle3 build-path=./webapp" $IMAGE_NAME
--- component/java/compile/jar/run.sh
+++ component/java/compile/jar/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 IMAGE_NAME=`cat imagename`
-docker run --rm --env CO_DATA="git-url=https://github.com/wangkirin/demo-bmi.git target=https://hub.opshub.sh/binary/v1/containerops/component/binary/2.2.4/demo.jar version=gradle3 build-path=./webapps" $IMAGE_NAME
+docker run --rm --env CO_DATA="git-url=https://github.com/wangkirin/demo-bmi.git target=https://hub.opshub.sh/binary/v1/containerops/component/binary/2.2.4/demo.jar version=gradle3 build-path=./calculator" $IMAGE_NAME
