e0fdcc5c9cf6ebe055e96771fb82cc262d37c7c3
liuzijie <fred.liu@ximalaya.com>
2017-11-02 17:46:46 +0800

    fix bugs

--- component/java/document/javadoc/run.sh
+++ component/java/document/javadoc/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 IMAGE_NAME=`cat imagename`
-docker run --rm --env CO_DATA="git-url=https://github.com/wangkirin/demo-bmi.git target=https://hub.opshub.sh/binary/v1/lidian/test/binary/1.1.0/javadoc.tar version=gradle3 build-path=./calculator out-put-type=xml" $IMAGE_NAME
+docker run --rm --env CO_DATA="git-url=https://github.com/wangkirin/demo-bmi.git target=https://hub.opshub.sh/binary/v1/lidian/test/binary/1.1.0/javadoc.tar version=gradle3 build-path=./calculator" $IMAGE_NAME
